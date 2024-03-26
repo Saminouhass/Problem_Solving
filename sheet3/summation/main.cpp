8bc4fce7e22c3093cd2ad2f766bdbc2b573791f2
OxSami <nouhasssami23@gmail.com>
2024-03-26 20:51:56 +0000

    code

--- sheet3/summation/main.cpp
+++ sheet3/summation/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
-
+    int n ;
+    long long sum = 0;
+    cin >> n;
+    int S[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> S[i];
+        sum += S[i];
+    }
+    cout << abs(sum);
     return 0;
 }
